a16206c7699b447ad13213b9c88e2b1598cf23a0
Ikenna <iykee0333@gmail.com>
2022-07-22 17:05:30 +0100

    built footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from 'react';
+import NextLink from 'next/link';
+import { Box, Container, Flex, Grid, UnorderedList, ListItem, Link, Text } from '@chakra-ui/react';
+
+import { AiFillFacebook, AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <footer>
-      Footer
+      <Box bg='blue.900' color='white'>
+        <Container maxW='container.xl' py={8}>
+          <Grid templateColumns={{ base: '1fr', md: 'repeat(3, 1fr)' }} alignItems='start'>
+            <Box>
+              <UnorderedList listStyleType='none'>
+                <ListItem fontSize='1.1rem' fontWeight='600' mb={2}>Links</ListItem>
+                <ListItem>
+                  <NextLink href='/'>
+                    <Link _hover={{ color: 'blue.400' }}>Home</Link>
+                  </NextLink>
+                </ListItem>
+                <ListItem>
+                  <NextLink href='/'>
+                    <Link _hover={{ color: 'blue.400' }}>Blog</Link>
+                  </NextLink>
+                </ListItem>
+                <ListItem>
+                  <NextLink href='/'>
+                    <Link _hover={{ color: 'blue.400' }}>About</Link>
+                  </NextLink>
+                </ListItem>
+              </UnorderedList>
+            </Box>
+            <Box>
+              <UnorderedList listStyleType='none'>
+                <ListItem fontSize='1.1rem' fontWeight='600' mb={2}>Company</ListItem>
+                <ListItem>
+                  <NextLink href='/'>
+                    <Link _hover={{ color: 'blue.400' }}>Support</Link>
+                  </NextLink>
+                </ListItem>
+                <ListItem>
+                  <NextLink href='/'>
+                    <Link _hover={{ color: 'blue.400' }}>Customer Care</Link>
+                  </NextLink>
+                </ListItem>
+                <ListItem>
+                  <NextLink href='/'>
+                    <Link _hover={{ color: 'blue.400' }}>Terms and Conditions</Link>
+                  </NextLink>
+                </ListItem>
+              </UnorderedList>
+            </Box>
+            <Flex align='center' gap='1rem'>
+              <Link color='white' _hover={{ color: 'blue.400' }}>
+                <AiFillFacebook fontSize='1.5rem' />
+              </Link>
+              <Link color='white' _hover={{ color: 'blue.400' }}>
+                <AiOutlineInstagram fontSize='1.5rem' />
+              </Link>
+              <Link color='white' _hover={{ color: 'blue.400' }}>
+                <AiOutlineTwitter fontSize='1.5rem' />
+              </Link>
+            </Flex>
+          </Grid>
+          <Box pt={8} textAlign='center'>
+            <Text color='#AAAAAA' fontSize='0.9rem'>&copy;{new Date().getFullYear()} BestBuy. All Rights Reserved</Text>
+          </Box>
+        </Container>
+      </Box>
     </footer>
   )
 }
